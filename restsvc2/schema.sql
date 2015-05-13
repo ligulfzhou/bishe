c235119bcfbe54c2d69000454d53ad0b9bfcc067
Ligulf Zhou <donsol53@gmail.com>
2015-05-13 23:16:45 +0800

    polish api, add api route

--- restsvc2/schema.sql
+++ restsvc2/schema.sql
@@ -79,17 +79,35 @@ create index i_tbgoods_cname on tbgoods using btree (cname);               -- wh
 --------------------------
 ------add fake data-------
 --------------------------
+-- 1: fake users
 insert into tbusers (nid, cname, cpassword) values (1, 'user1', 'user1');
 insert into tbusers (nid, cname, cpassword) values (2, 'user2', 'user2');
 insert into tbusers (nid, cname, cpassword) values (3, 'user3', 'user3');
 insert into tbusers (nid, cname, cpassword) values (4, 'user4', 'user4');
 
-insert into tbcategories (nid, cname) values (1, 'books', 0);
-insert into tbcategories (nid, cname) values (2, 'mobile phones', 0);
-insert into tbcategories (nid, cname) values (3, 'computers', 0);
-insert into tbcategories (nid, cname) values (4, 'condem', 0);
+-- 2: fake categories
+insert into tbcategories (nid, cname) values (1, 'books');
+insert into tbcategories (nid, cname) values (2, 'mobile phones');
+insert into tbcategories (nid, cname) values (3, 'computers');
+insert into tbcategories (nid, cname) values (4, 'condem');
 
-insert into tbcategories (nid, cname) values (1, 'books', 0);
-insert into tbcategories (nid, cname) values (2, 'mobile phones', 0);
-insert into tbcategories (nid, cname) values (3, 'computers', 0);
-insert into tbcategories (nid, cname) values (4, 'condem', 0);
+-- 3: fake goods
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (1, 'java in action', 20, 'it is about java language', 1, 1000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (2, 'erlang otp in action', 30, 'it is about erlang language and otp framework', 1, 2000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (3, 'definitive guide to cocos2d-x', 40, 'it talks about the cocos2d-x game engine', 1, 3000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (4, 'introduction to tornado', 50, 'about python/tornado framework', 1, 4000);
+
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (5, 'nokia 5250', 800, 'cheap, last long', 2, 1000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (6, 'iphone 6', 6000, 'amazing, apple tech', 2, 2000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (7, 'huawei c353', 1200, 'lalala', 2, 3000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (8, 'samsung z7', 1700, 'so so', 2, 4000);
+
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (9, 'nokia 5250', 800, 'cheap, last long', 3, 1000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (10, 'iphone 6', 6000, 'amazing, apple tech', 3, 2000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (11, 'huawei c353', 1200, 'lalala', 3, 3000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (12, 'samsung z7', 1700, 'so so', 3, 4000);
+
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (13, 'lenovo v470', 5000, 'cheap, last long', 4, 1000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (14, 'mac pro', 12000, 'fantastic', 4, 2000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (15, 'mac air', 8000, 'awesome', 4, 3000);
+insert into tbgoods (nid, cname, fprice, cdesc, ncategoryid, ncount) values (16, 'chromium', 3000, 'cheap', 4, 4000);
--- restsvc2/urls.py
+++ restsvc2/urls.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from handlers import test
+from handlers import test, categories, goods, login, users, categoriedgoods
 
 url_patterns = [
-    (r"/", test.TestHandler),
-    (r"/(\d+)/(orders)$", test.OrdersHandler)
+    (r"/api/v1", test.TestHandler),
+    (r"/(\d+)/(orders)$", test.OrdersHandler),
+
+    (r"/api/v1/categories", categories.CategoriesHandler),
+    (r"/api/v1/categories/(\d+)", categories.CategoryHandler),
+
+    (r"/api/v1/goods", goods.GoodsHandler),
+    (r"/api/v1/categories/(\d+)/goods", categoriedgoods.CategoriedGoodsHandler),
+    (r"/api/v1/goods/(\d+)", goods.GoodHandler),
+
+    (r"/api/v1/login", login.LoginHandler),
+
+    (r"/api/v1/users", users.UsersHandler),
+    (r"/api/v1/users/(\d+)", users.UserHandler),
+
+    (r"/api/v1/orders", orders.OrdersHandler),
+    (r"/api/v1/orders/(\d+)", orders.OrderHandler)
+    #(r"/api/v1/users/(\d+)/orders")
 ]
