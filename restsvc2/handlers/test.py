20b686890c696f9e4a4d5d0d177fa50d440051da
Ligulf Zhou <donsol53@gmail.com>
2015-06-25 20:01:59 +0800

    fix tab and space

--- restsvc2/handlers/test.py
+++ restsvc2/handlers/test.py
@@ -7,38 +7,38 @@ class TestHandler(BaseHandler):
 
     @login_required
     def get(self):
-    	'''
-    	get favorite goods, return goods list
-    	'''
+        '''
+        get favorite goods, return goods list
+        '''
         # return self.write("hello world:{0}".format(self.current_user.get("nid")))
         userid = self.current_user['nid']
         cursor = self.conn.cursor()
         try:
-        	cursor.execute("select a.nid, a.cname, a.dprice, a.cdesc, a.ncategoryid, a.count \
-        					from tbgoods as a, tbfavorites as b \
-        					where a.nid=b.ngood_id and b.nuser_id={0}".foramt(userid))
+            cursor.execute("select a.nid, a.cname, a.dprice, a.cdesc, a.ncategoryid, a.count \
+                            from tbgoods as a, tbfavorites as b \
+                            where a.nid=b.ngood_id and b.nuser_id={0}".foramt(userid))
         except:
-        	raise HTTPError(500)
+            raise HTTPError(500)
 
         if cursor.rowcount > 0:
-        	goods = cursor.fetchall()
-        	cursor.close()
-        	goods_json = [{"nid":good[0],
-        					"cname": good[1],
-        					"dprice": good[2],
-        					"cdesc": good[3],
-        					"ncategoryid": good[4],
-        					"count": good[5]}
-        					for good in goods]
-        	return self.write(json.dumps(goods_json))
+            goods = cursor.fetchall()
+            cursor.close()
+            goods_json = [{"nid":good[0],
+                            "cname": good[1],
+                            "dprice": good[2],
+                            "cdesc": good[3],
+                            "ncategoryid": good[4],
+                            "count": good[5]}
+                            for good in goods]
+            return self.write(json.dumps(goods_json))
         else:
-        	return self.write(json.dumps([]))
+            return self.write(json.dumps([]))
 
-  	@login_required
-  	def post(self):
+    @login_required
+    def post(self):
         goodid = self.get_argument('goodid', 0)
         if goodid == 0:
-            raise HTTPError(501)
+            raise HTTPError(500)
 
         print goodid
         userid = self.current_user['nid']
